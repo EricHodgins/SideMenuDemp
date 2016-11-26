d090a75a663c7df747a4265d6f6c50af3b4ef878
Eric Hodgins <hodgins.e@gmail.com>
2016-11-26 17:27:57 -0500

    feat: Setup menuItem class for random items for side menu.

--- SideMenuDemo/MenuItem.swift
+++ SideMenuDemo/MenuItem.swift
@@ -9,5 +9,33 @@
 import UIKit
 
 class MenuItem {
+    let title: String
+    let color: UIColor
     
+    init(title: String, color: UIColor) {
+        self.title = title
+        self.color = color
+    }
+    
+    class func demoItems(numberOfItems: Int) -> [MenuItem] {
+        var items = [MenuItem]()
+        
+        for i in 1...numberOfItems {
+            let item = MenuItem(title: "\(i)", color: UIColor.randomColor())
+            items.append(item)
+        }
+        
+        return items
+    }
+}
+
+
+extension UIColor {
+    class func randomColor() -> UIColor {
+        let randomRed = CGFloat(arc4random_uniform(10)) / 10.0
+        let randomGreen = CGFloat(arc4random_uniform(10)) / 10.0
+        let randomBlue = CGFloat(arc4random_uniform(10)) / 10.0
+        
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    }
 }
