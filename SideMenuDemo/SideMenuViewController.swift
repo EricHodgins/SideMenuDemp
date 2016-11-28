460de5cc832bbf3f5c614434e0b47a46e0617283
Eric Hodgins <hodgins.e@gmail.com>
2016-11-27 22:07:35 -0500

    feat: Added animation on menu cell when it is touched. White oval expands and reverses real quick.

--- SideMenuDemo/SideMenuViewController.swift
+++ SideMenuDemo/SideMenuViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class SideMenuViewController: UITableViewController {
     
     var centerViewController: CenterViewController!
-
+    let circleLayer = CAShapeLayer()
 
     // MARK: - Table view data source
 
@@ -38,9 +38,37 @@ class SideMenuViewController: UITableViewController {
         
         let containerViewController = parent as! ContainerViewController
         containerViewController.toggleSideMenu()
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        let cellBounds = cell!.contentView.bounds
+        var rect = CGRect()
+        rect.size = CGSize(width: cellBounds.size.width/4, height: cellBounds.size.height/4)
+        rect.origin.x = cell!.contentView.center.x - rect.size.width/2
+        rect.origin.y = cell!.contentView.center.y - rect.size.height/2
+        circleLayer.path = UIBezierPath(ovalIn: rect).cgPath
+        circleLayer.strokeColor = UIColor.white.cgColor
+        circleLayer.fillColor = UIColor.white.cgColor
+        cell?.contentView.layer.addSublayer(circleLayer)
+        
+        let morphCircleAnim = CABasicAnimation(keyPath: "path")
+        morphCircleAnim.delegate = self
+        morphCircleAnim.setValue(circleLayer, forKey: "layer")
+        morphCircleAnim.autoreverses = true
+        morphCircleAnim.duration = 0.1
+        morphCircleAnim.toValue = UIBezierPath(ovalIn: cellBounds).cgPath
+        
+        circleLayer.add(morphCircleAnim, forKey: nil)
     }
 
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return view.frame.height / CGFloat(MenuItem.sharedItems.count)
+    }
+}
+
+extension SideMenuViewController: CAAnimationDelegate {
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        let circleLayer = anim.value(forKey: "layer") as? CALayer
+        anim.setValue(nil, forKey: "layer")
+        circleLayer?.removeFromSuperlayer()
     }
 }
